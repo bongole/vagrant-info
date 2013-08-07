5affdac9a584bfdf5c383a396635547d4919586b
bongole <bongole2@gmail.com>
2013-08-07 18:26:46 +0900

    Add show guest IP addresses

--- lib/vagrant-info/command/info.rb
+++ lib/vagrant-info/command/info.rb
@@ -17,7 +17,16 @@ module VagrantInfo
         with_target_vms(argv) do |machine|
           result = ""
           result << machine.name.to_s << "\n"
-          result << JSON.pretty_generate(machine.config.vm)
+          result << JSON.pretty_generate(machine.config.vm) << "\n"
+          if machine.provider_name == :virtualbox
+              info = machine.provider.driver.execute('guestproperty', 'enumerate', machine.id)
+              iplines = info.split("\n").grep(/Net.*V4.*IP/)
+              iplines.each do |line|
+                  if line =~ %r{Net/(.)/V4/IP, value: (.*?),}
+                     result << "IP#{$1}: #{$2}" << "\n"
+                  end
+              end
+          end
           results << result
         end
 
